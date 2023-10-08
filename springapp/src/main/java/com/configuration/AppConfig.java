181fe33bbadd98930534b3102f6edb12b642f3c8
Iamneo <techadmin@iamneo.ai>
2023-10-08 09:17:50 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/configuration/AppConfig.java
@@ -0,0 +1,18 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import your.package.Student;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Student student() {
+        Student student = new Student();
+        student.setStudentId(1);
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        return student;
+    }
+}
